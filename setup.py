52fe3e9a0700ec0f1e43e0fa1bdfe3b2fe4b795f
Chigozie mbonu <chigozie.mbonu@gmail.com>
2022-01-18 14:07:52 +0100

    My second addition

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(  
-        name = 'summarizermodule',
+        name = 'mysummarizer',
         version = '1.0',
         packages = find_packages(exclude=['tests*']),
         license = 'MIT',
